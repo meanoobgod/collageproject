c3d81fc4ee2d281fa1a828b5464ee3126c7ff247
Rohit <meanoobgod@gmail.com>
2026-09-16 11:59:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,6 +133,7 @@ def GetResultTeacher():
                 "getresultteacher.html", 
                 student_results=student_results
                 )
+    return "<h1 style='background: black; color: white;'>No Record found Found, either the paper id is invalid or you forgot your password</h1>"
 
 
 @app.route("/student/getresult", methods=["GET"])
